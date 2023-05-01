da461f046dd7cc37b78dcd9f0c0654fb547a92c8
jaegglic <christoph.jaeggli@swissmedic.ch>
2023-05-01 14:18:03 +0200

    Made first version of streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,65 +13,44 @@ API_KEY = os.environ.get("OPENAI_API_KEY", "")
 st.title("NER by GPT-4")
 
 
-def initialize_messages():
+def prompting(text: str) -> List[dict]:
     # Taken from here
     # https://platform.openai.com/docs/guides/chat/introduction
-    sys_msg = 'Identify named entities "MEDICINAL PRODUCT" and "ADVERSE EVENT" from a given text ' \
-              'and return it in a JSON format.'
-    shots = [
-        {
-            'text': "Treatment with ipilimumab has been linked to the rare yet serious adverse "
-                    "event of serious retinal detachment. The amount of photoreceptor degeneration "
-                    "and loss of vision can be minimised by early diagnosis and treatment.",
-            'ner': [
-                {'T': 'MEDICINAL PRODUCT', 'E': 'ipilimumab'},
-                {'T': 'ADVERSE EVENT', 'E': 'serous retinal detachment'},
-                {'T': 'ADVERSE EVENT', 'E': 'loss of vision'}],
-        },
-        {
-            'text': "Clindamycin is potentially nephrotoxic. Acute kidney injury including acute "
-                    "renal failure has been reported.",
-            'ner': [
-                {'T': 'MEDICINAL PRODUCT', 'E': 'Clindamycin'},
-                {'T': 'ADVERSE EVENT', 'E': 'Acute kidney injury'},
-                {'T': 'ADVERSE EVENT', 'E': 'renal failure'}],
-        }
-    ]
+    sys_msg = 'Act as a medical professional who summarizes technical literature for ' \
+              'non-medical people in an understandable and simple way. Do not add any ' \
+              'additional information and do not comment the reviewed text. The only task you ' \
+              'do is to output the rewritten and simplified text.'
 
     messages = [{'role': 'system', 'content': sys_msg}]
-    for shot in shots:
-        messages.append({'role': 'user', 'content': shot['text']})
-        messages.append({'role': 'assistant', 'content': json.dumps(shot['ner'])})
+    text = f'Do the above simplification task with the following text: "{text}"'
+    messages.append({'role': 'user', 'content': text})
     return messages
 
 
-def gpt_4(messages: List):
+def gpt_4(messages: List[dict]) -> str:
     """This function generate a response to the given query using gpt-4."""
     response = openai.ChatCompletion.create(
         model="gpt-4",
         messages=messages,
         api_key=API_KEY,
     )
-    msg = {'role': 'assistant', 'content': response.choices[0].message['content']}
-    return msg
+    return response.choices[0].message['content']
 
 
 def main():
     """This function gets the user input, pass it to ChatGPT function and
     displays the response
     """
-    messages = initialize_messages()
     # Get user input
-    query_text = st.text_input(label='enter your text...')
-    if query_text:
-        user_query = {'role': 'user', 'content': query_text}
-        messages.append(user_query)
-        st.markdown(query_text)
+    text = st.text_input(label='enter your text...')
+    if text:
+        messages = prompting(text=text)
+        st.markdown(text)
+
+        st.markdown("---")
 
         response = gpt_4(messages=messages)
-        messages.append(response)
-        ner = json.loads(response['content'])
-        st.json(ner)
+        st.markdown(response)
 
 
 if __name__ == '__main__':
